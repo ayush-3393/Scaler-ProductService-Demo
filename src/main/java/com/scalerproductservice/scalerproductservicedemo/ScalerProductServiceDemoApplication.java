2fedbc44264d791afb63c0b8448efebc66af6ff9
ayushsinha3393 <ayush.sinha3393@gmail.com>
2023-09-23 10:47:10 +0530

    Initialising Project

---
+++ src/main/java/com/scalerproductservice/scalerproductservicedemo/ScalerProductServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.scalerproductservice.scalerproductservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerProductServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerProductServiceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scalerproductservice/scalerproductservicedemo/ScalerProductServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalerproductservice.scalerproductservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerProductServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
